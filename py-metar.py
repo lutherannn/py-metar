1225f351069f3a793a2660f822710a8e4305aaaa
lutherannn <michael.eskelin@gmail.com>
2022-05-18 23:18:39 -0400

    Deleted extra files, refactor

--- py-metar.py
+++ py-metar.py
@@ -2,19 +2,21 @@ import requests
 import json
 import sys
 
+
 class colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
 
 def main():
-    #Check for no args
+    # Check for no args
     if len(sys.argv) < 2:
         print("Not enough arguments, see --help")
         return 0
@@ -56,4 +58,6 @@ def main():
         # Print final result
         print(f"METAR for: {colors.WARNING}{i.upper()}{colors.ENDC}")
         print(f"{colors.OKGREEN} {result} \n {colors.ENDC}")
+
+
 main()
